7ee23964356f4386d7e404da304f1984689ba0d1
ariumaschi <aumaschi@udesa.edu.ar>
2024-08-17 14:37:26 -0300

    Login auth0

--- thumbnail-generator-ui/sinapsis-arianaumaschi/src/App.tsx
+++ thumbnail-generator-ui/sinapsis-arianaumaschi/src/App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import './App.css';
 import ImageUploader from './components/ImageUploader';
+import { useAuth0 } from '@auth0/auth0-react';
 
-function App() {
+const App: React.FC = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
   return (
     <div className="App">
-      <h1>Generador de Thumbnails</h1>
-      <ImageUploader />
+      <header className="App-header">
+        <h1>Generador de Thumbnails</h1>
+        {isAuthenticated ? (
+          <>
+            <button
+              onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}
+            >
+              Logout
+            </button>
+            <ImageUploader />
+          </>
+        ) : (
+          <button onClick={() => loginWithRedirect()}>Login</button>
+        )}
+      </header>
     </div>
   );
-}
+};
 
 export default App;
--- thumbnail-generator-ui/sinapsis-arianaumaschi/src/index.tsx
+++ thumbnail-generator-ui/sinapsis-arianaumaschi/src/index.tsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-bxhujwaa6h8ork01.us.auth0.com"
+    clientId="DvknbK6zyEpzYTg9IVD2CPi7DV7XQWst"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
