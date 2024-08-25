c276900b48dfbdc41af9cb2246678059bb459d97
ariumaschi <aumaschi@udesa.edu.ar>
2024-08-25 09:21:15 -0300

    cropping area / resizes helper

--- thumbnail-generator-ui/sinapsis-arianaumaschi/src/components/ImageUploader.tsx
+++ thumbnail-generator-ui/sinapsis-arianaumaschi/src/components/ImageUploader.tsx
@@ -1,6 +1,8 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import { uploadImage } from '../services/thumbnailService';
 import { Box, Button, Typography, Input, ImageList, ImageListItem, ImageListItemBar, CircularProgress, useMediaQuery, useTheme } from '@mui/material';
+import ReactCrop, { Crop } from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
 
 const ImageUploader: React.FC = () => {
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
@@ -8,6 +10,15 @@ const ImageUploader: React.FC = () => {
     const [thumbnailUrls, setThumbnailUrls] = useState<{ url: string; image: string }[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const [isDragging, setIsDragging] = useState<boolean>(false);
+    const [crop, setCrop] = useState<Crop>({
+        unit: '%', // Puede ser 'px' o '%'
+        x: 25,
+        y: 25,
+        width: 50,
+        height: 50,
+    }); const [completedCrop, setCompletedCrop] = useState<Crop | null>(null);
+    const [croppedImageUrl, setCroppedImageUrl] = useState<string | null>(null);
+    const imageRef = useRef<HTMLImageElement | null>(null);
     const fileInputRef = useRef<HTMLInputElement | null>(null);
 
     const theme = useTheme();
@@ -21,25 +32,98 @@ const ImageUploader: React.FC = () => {
             setPreviewUrl(URL.createObjectURL(file));
         }
     };
-
     const handleUpload = async () => {
-        if (selectedFile) {
+        if (croppedImageUrl) {
+            console.log("croppedImageUrl ok");
             setLoading(true);
             try {
-                setPreviewUrl(null);
-                setSelectedFile(null);
+                // Establecer la URL de vista previa recortada
+                setPreviewUrl(croppedImageUrl);
+
+                // Verificar si selectedFile está disponible y válido antes de continuar
+                if (!selectedFile) {
+                    throw new Error('No se ha seleccionado ningún archivo.');
+                }
+
+                // Limpiar el valor del input de archivo
                 if (fileInputRef.current) {
                     fileInputRef.current.value = '';
                 }
-                const { url, image } = await uploadImage(selectedFile);
+
+                // Convertir la URL de la imagen recortada en un archivo
+                const croppedFile = await fetch(croppedImageUrl)
+                    .then((r) => r.blob())
+                    .then((blobFile) => new File([blobFile], selectedFile.name, { type: selectedFile.type }));
+
+                // Subir la imagen recortada
+                const { url, image } = await uploadImage(croppedFile);
                 setThumbnailUrls((prevUrls) => [...prevUrls, { url, image }]);
             } catch (error) {
                 console.error('Error al subir la imagen:', error);
             } finally {
                 setLoading(false);
             }
+        } else {
+            console.error('Error: No se ha recortado ninguna imagen.');
         }
     };
+
+    const onImageLoaded = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+        const img = e.currentTarget; // Accedes al elemento img desde el evento
+        // Resto del código para manejar la imagen cargada
+    };
+
+    const onCropComplete = (crop: Crop) => {
+        setCompletedCrop(crop);
+    };
+
+    const onCropChange = (crop: Crop) => {
+        setCrop(crop);
+    };
+
+    const generateCroppedImage = useCallback(() => {
+        console.log('generating')
+        console.log('Estado de completedCrop:', completedCrop);
+        console.log('Estado de imageRef.current:', imageRef.current);
+        if (!completedCrop || !imageRef.current) {
+            console.log('completedCrop o imageRef.current son inválidos');
+
+            return;
+        }
+        console.log('Intentando crear el canvas');
+        const canvas = document.createElement('canvas');
+        console.log(canvas); // Verificar si el canvas fue creado
+
+        const scaleX = imageRef.current.naturalWidth / imageRef.current.width;
+        const scaleY = imageRef.current.naturalHeight / imageRef.current.height;
+        canvas.width = completedCrop.width!;
+        canvas.height = completedCrop.height!;
+        const ctx = canvas.getContext('2d');
+
+        if (ctx) {
+
+            ctx.drawImage(
+                imageRef.current,
+                completedCrop.x! * scaleX,
+                completedCrop.y! * scaleY,
+                completedCrop.width! * scaleX,
+                completedCrop.height! * scaleY,
+                0,
+                0,
+                completedCrop.width!,
+                completedCrop.height!
+            );
+
+            canvas.toBlob((blob) => {
+                console.log(blob);
+                if (blob) {
+                    setCroppedImageUrl(URL.createObjectURL(blob));
+                    console.log(croppedImageUrl)
+                }
+            }, 'image/jpeg');
+        }
+    }, [completedCrop]);
+
 
     const handleFileDrop = (file: File) => {
         setSelectedFile(file);
@@ -64,68 +148,65 @@ const ImageUploader: React.FC = () => {
     };
 
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                width: '100%',
-                p: 2,
-                borderRadius: 2,
-                textAlign: 'center',
-                mt: 4,
-                backgroundColor: isDragging ? 'lightblue' : 'aqua',
-                border: isDragging ? '2px dashed #000' : '2px solid #000',
-                transition: 'background-color 0.3s, border 0.3s',
-                cursor: 'pointer',
-            }}
+        <Box sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '100%',
+            p: 2,
+            borderRadius: 2,
+            textAlign: 'center',
+            mt: 4,
+            backgroundColor: isDragging ? 'lightblue' : 'aqua',
+            border: isDragging ? '2px dashed #000' : '2px solid #000',
+            transition: 'background-color 0.3s, border 0.3s',
+            cursor: 'pointer',
+        }}
             onDragOver={handleDragOver}
             onDragLeave={handleDragLeave}
-            onDrop={handleDrop}
-        >
+            onDrop={handleDrop}>
             <Typography variant="h6" sx={{ mb: 2 }}>
                 {isDragging ? 'Suelta el archivo aquí' : 'Arrastra y suelta un archivo o selecciona uno'}
             </Typography>
 
-            <Input
-                type="file"
-                onChange={handleFileChange}
-                inputRef={fileInputRef}
-                sx={{ display: 'none' }}
-            />
-            <Button
-                variant="contained"
-                component="label"
-                sx={{ mb: 2 }}
-            >
+            <Input type="file" onChange={handleFileChange} inputRef={fileInputRef} sx={{ display: 'none' }} />
+            <Button variant="contained" component="label" sx={{ mb: 2 }}>
                 Seleccionar archivo
-                <Input
-                    type="file"
-                    onChange={handleFileChange}
-                    inputRef={fileInputRef}
-                    sx={{ display: 'none' }}
-                />
+                <Input type="file" onChange={handleFileChange} inputRef={fileInputRef} sx={{ display: 'none' }} />
             </Button>
 
             {previewUrl && (
                 <Box sx={{ mb: 2 }}>
-                    <Typography variant="h6">Vista previa:</Typography>
-                    <img
-                        src={previewUrl}
-                        alt="Vista previa"
-                        style={{ maxWidth: '100%', maxHeight: '40vh' }}
-                    />
+                    <Typography variant="h6">Recortar Imagen:</Typography>
+                    <ReactCrop
+                        crop={crop}
+                        onChange={(newCrop) => setCrop(newCrop)}
+                        onComplete={(c) => setCompletedCrop(c)}
+                    >
+                        <img
+                            ref={imageRef}
+
+                            src={previewUrl}
+                            alt="Preview"
+                            onLoad={onImageLoaded}
+                        />
+                    </ReactCrop>
+                    <Button variant="contained" color="primary" onClick={generateCroppedImage} sx={{ mt: 2 }}>
+                        Generar Imagen Recortada
+                    </Button>
                 </Box>
             )}
 
-            {selectedFile && !loading && (
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleUpload}
-                    sx={{ mb: 4 }}
-                >
-                    Subir Imagen
+            {croppedImageUrl && (
+                <Box sx={{ mb: 2 }}>
+                    <Typography variant="h6">Vista previa de la imagen recortada:</Typography>
+                    <img src={croppedImageUrl} alt="Cropped Preview" style={{ maxWidth: '100%', maxHeight: '40vh' }} />
+                </Box>
+            )}
+
+            {croppedImageUrl && !loading && (
+                <Button variant="contained" color="primary" onClick={handleUpload} sx={{ mb: 4 }}>
+                    Subir Imagen Recortada
                 </Button>
             )}
 
@@ -134,7 +215,6 @@ const ImageUploader: React.FC = () => {
                     <CircularProgress />
                 </Box>
             )}
-
             <Box
                 sx={{
                     display: 'flex',
